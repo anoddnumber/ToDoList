8df3d01fa0e1118bb303a84ecab80d0381a5758e
anoddnumber <samsonwong@samsons-mbp.attlocal.net>
2014-07-25 19:08:36 -0700

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Samson Wong on 7/25/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
